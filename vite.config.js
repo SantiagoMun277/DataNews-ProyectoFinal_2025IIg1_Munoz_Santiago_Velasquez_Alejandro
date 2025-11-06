4b335fe28d3dfd527888d9752c6725b3ec20e2d7
SantiagoMun277 <munozsantiago277@gmail.com>
2025-11-06 18:16:15 -0500

    cambio4

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-const isVercel = !!process.env.VERCEL
+const isVercel = process.env.VERCEL === '1' || process.env.VERCEL === 'true'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
