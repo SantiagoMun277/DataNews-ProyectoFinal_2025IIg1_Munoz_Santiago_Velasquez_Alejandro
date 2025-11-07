9446db27a7f47787e1b7b7792babcea47a436045
SantiagoMun277 <munozsantiago277@gmail.com>
2025-11-06 19:14:25 -0500

    cambi5

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// const isVercel = process.env.VERCEL === '1' || process.env.VERCEL === 'true'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//      base: isVercel
+//     ? '/' // Vercel
+//     : '/DataNews-ProyectoFinal_2025IIg1_Munoz_Santiago_Velasquez_Alejandro/', // GitHub Pages
+// })
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-const isVercel = process.env.VERCEL === '1' || process.env.VERCEL === 'true'
-// https://vite.dev/config/
+// Solo activamos base de GitHub Pages cuando GHPAGES=1
+const isGhPages = process.env.GHPAGES === '1'
 export default defineConfig({
   plugins: [react()],
-     base: isVercel
-    ? '/' // Vercel
-    : '/DataNews-ProyectoFinal_2025IIg1_Munoz_Santiago_Velasquez_Alejandro/', // GitHub Pages
+  base: isGhPages
+    ? '/DataNews-ProyectoFinal_2025IIg1_Munoz_Santiago_Velasquez_Alejandro/'
+    : '/', // <- Vercel y dev local
 })
